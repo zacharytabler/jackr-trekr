3da31aef2b6d2baca7ee26c1424787777e24cbaf
nbillhardt <nickbillhardt@ymail.com>
2021-06-06 15:45:40 -0400

    Recommit

---
+++ src/main/java/com/jackrtrekr/jackrtrekrteam3/JackrTrekrTeam3Application.java
@@ -0,0 +1,13 @@
+package com.jackrtrekr.jackrtrekrteam3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackrTrekrTeam3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackrTrekrTeam3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackrtrekr/jackrtrekrteam3/JackrTrekrTeam3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackrtrekr.jackrtrekrteam3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackrTrekrTeam3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
